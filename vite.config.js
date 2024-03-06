0520316c0e40a6e3dc6efd7b812fb7f7bcd9ecd3
Cecelia Bloom <crbloom01@gmail.com>
2024-03-06 10:36:33 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://CeceliaBloom.github.io/final_project/",
+  base: "https://CeceliaBloom.github.io",
   plugins: [react()],
 })
